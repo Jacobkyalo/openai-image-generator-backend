1fd31847db88a770b05b8933d80b955be70daeee
Jacob Kyalo <jacobkyalo43@gmail.com>
2023-03-31 10:14:51 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const cors = require("cors");
+const { Configuration, OpenAIApi } = require("openai");
+require("dotenv").config();
+
+// OPENAI CONFIGURATION
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+
+// APP INIT
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// EXPRESS MIDDLEWARES
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// APP POST ENDPOINT
+app.post("/", async (req, res) => {
+  const { prompt, amount } = req.body;
+
+  try {
+    const response = await openai.createImage({
+      prompt: `${prompt}`,
+      n: Number(`${amount}`),
+      size: "1024x1024",
+    });
+    const imageURLS = response.data.data;
+    res.status(200).json({ images: imageURLS });
+  } catch (error) {
+    res.status(500).json({ error: "Sorry, something went wrong! ☹️" });
+  }
+});
+
+// APP LISTENING
+app.listen(PORT, () => {
+  console.log(`Server running at port ${PORT}`);
+});
